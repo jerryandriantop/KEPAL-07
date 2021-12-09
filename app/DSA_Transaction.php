d20fdb9b20e63487a644889f5f95762698d0b04f
andreaszico <zicoaritonang@gmail.com>
2021-12-09 20:44:36 +0700

    fix

--- app/DSA_Transaction.php
+++ app/DSA_Transaction.php
@@ -266,7 +266,7 @@ class DSA_Transaction
                         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                             <tr>
                                 <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: "Source Sans Pro", Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-                                    <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Thank you for your order! ' . $res['firstname'] . ' ' . $res['middlename'] . ' ' . $res['lastname'] . '</h1>
+                                    <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Thank you for your order! <br>' . $res['firstname'] . ' ' . $res['middlename'] . ' ' . $res['lastname'] . '</h1>
                                 </td>
                             </tr>
                         </table>
